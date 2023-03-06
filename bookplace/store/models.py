4bab043352ef5ae43dc249844ea63d84d108bdac
Perun951 <tudor.boana@gmail.com>
2023-03-06 22:11:56 +0200

    dgnhfxbs

--- bookplace/store/models.py
+++ bookplace/store/models.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import User
 from django.core.validators import FileExtensionValidator
 from django.db import models
+from django.core.files import File
+
+from io import BytesIO
+from PIL import Image
 
 class Category(models.Model):
     title = models.CharField(max_length=50)
@@ -29,6 +33,7 @@ class Product(models.Model):
     category=models.ForeignKey(Category, related_name='products',on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     pdf = models.FileField(upload_to='uploads/pdfs/', blank=False, null=False ,  validators=[FileExtensionValidator(allowed_extensions=["pdf","epub"])])
+    thumbnail = models.ImageField(upload_to='uploads/product_images/thumbnails/', blank=True, null= True)
     image = models.ImageField(upload_to='uploads/product_images/', blank=True, null=True)
     editie = models.IntegerField()
     editura = models.CharField(max_length=50)
@@ -44,6 +49,28 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_thumbnail(self):
+        if self.thumbnail:
+            return self.thumbnail.url
+        else:
+            if self.image:
+                self.thumbnail = self.make_thumbnail(self.image)
+                self.save()
+
+                return self.thumbnail.url
+            else:
+                return 'media/photo_2023-02-08_13-03-17.jpg'
+
+    def make_thumbnail(self,image,size=(350,350)):
+        img = Image.open(image)
+        img.convert('RGB')
+        img.thumbnail(size)
+
+        thumb_io = BytesIO()
+        img.save(thumb_io, 'JPEG',quality=85)
+        thumbnail=File(thumb_io,name=image.name)
+        return thumbnail
 
     def get_rating(self):
         reviews_total = 0
